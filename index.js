8c4533ff3d7ebd54255ee8e00b207f91add14d9b
Aiden Zafar <+>
2022-09-19 19:08:01 -0400

    commit tree e016206d6b986651d38b7a8413c8eef92d103426

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance > 2500){
+    return 'No can do.'
+  }
+  else {
+    if (distance <= 400){
+    return 'This one is on me!'
+    }
+    if (distance <= 2000){
+    return 'That will be twenty bucks.';
+    }
+    if (distance > 2000){
+    return 'I will gladly take your thirty bucks.'
+    }
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(location){
+  if (location === 'NYC'){
+    return 'Ok, sounds good.'
+  }
+  else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(amount){
+  if (amount === 'generous'){
+    return 'Thank you so much.'
+  }
+  else {
+    if (amount === 'not as generous'){
+      return 'Thank you.'
+    }
+    else {
+      return 'Bye.'
+    }
+  }
 }
